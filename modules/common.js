1194bd5bc1cd876b34358ae18b4621891e285a91
marielinkepp <marielin.kepp@ut.ee>
2024-01-26 10:25:33 +0200

    Update common.js
    fixed js function error

--- modules/common.js
+++ modules/common.js
@@ -1,7 +1,7 @@
 function applyTemplate(templateName) {
-
-    const content = fetch(`./templates/${templateName}.html`)
-        .then(response => response.text());
-  
-    document.getElementById('content').innerHTML = content;
-  }
+    fetch(`./templates/${templateName}.html`)
+        .then(response => response.text())
+        .then(content => {
+            document.getElementById('content').innerHTML = content;
+        });
+}
