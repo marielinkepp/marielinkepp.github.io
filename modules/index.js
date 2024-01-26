3378830e201df88218e572c28a5b42cacba92338
marielinkepp <marielin.kepp@ut.ee>
2024-01-26 10:59:20 +0200

    test console logging

--- modules/index.js
+++ modules/index.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     var navMenuDropdown = document.getElementById("nav-menu-dropdown");
     console.log(navMenuDropdown);
     navMenuDropdown.addEventListener("click", function() {
+        console.log(True);
         var nav = document.getElementById("nav-links");
         if (nav.style.display === "none") {
             nav.style.display = "block";
