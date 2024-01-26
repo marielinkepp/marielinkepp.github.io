8fa68e8c60e5b2dd898a6702167d8375ed3b5031
marielinkepp <marielin.kepp@ut.ee>
2024-01-26 10:51:47 +0200

    replaced toggleNav with a window event listener

--- modules/index.js
+++ modules/index.js
@@ -1,26 +1,11 @@
-//import {navigateTo} from "./common.js";
-
-function toggleNav() {
-    var nav = document.getElementById("nav-links");
-    if (nav.style.display === "none") {
-        nav.style.display = "block";
-    } else {
-        nav.style.display = "none";
-    }
-  }
-  
-/*selectContent();
-
-function selectContent() {
-    const pageId = new URLSearchParams(window.location.search)
-        .get('page') || 'index';
-
-    const itemId = new URLSearchParams(window.location.search).get('id');
-
-    console.log(pageId);
-    console.log(itemId);
-
-    //applyTemplate(pageId);
-    navigateTo('?page=' + pageId);
-
-}*/
+window.addEventListener('DOMContentLoaded', (event) => {
+    var navMenuDropdown = document.getElementById("nav-menu-dropdown");
+    navMenuDropdown.addEventListener("click", function() {
+        var nav = document.getElementById("nav-links");
+        if (nav.style.display === "none") {
+            nav.style.display = "block";
+        } else {
+            nav.style.display = "none";
+        }
+    });
+});
